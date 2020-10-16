0209b91fdbb04951066bdd2155dfe5b57cfc60ad
Jonathan Geffen <t-jogeff@microsoft.com>
2020-10-16 15:50:14 +0200

    support moving player with mouse clicks

--- PrivateInvestigators/Assets/Mapbox/Examples/6_ZoomableMap/Scripts/SpawnOnMap.cs
+++ PrivateInvestigators/Assets/Mapbox/Examples/6_ZoomableMap/Scripts/SpawnOnMap.cs
@@ -35,6 +35,7 @@
 				var locationString = _locationStrings[i];
 				_locations[i] = Conversions.StringToLatLon(locationString);
 				var instance = Instantiate(_markerPrefab);
+				instance.SetActive(false);
 				instance.transform.localPosition = _map.GeoToWorldPosition(_locations[i], true);
 				instance.transform.localScale = new Vector3(_spawnScale, _spawnScale, _spawnScale);
 				_spawnedObjects.Add(instance);
