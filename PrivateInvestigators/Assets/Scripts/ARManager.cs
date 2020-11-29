ab08eb53f6f5d2a4b9dda5c2ed294127d6bcb232
Gabriel R <Riotzz42@gmail.com>
2020-11-29 20:56:03 +0100

    AR tutorial and placement

--- PrivateInvestigators/Assets/Scripts/ARManager.cs
+++ PrivateInvestigators/Assets/Scripts/ARManager.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
-
+using UnityEngine.EventSystems;
 
 
 [RequireComponent(typeof(ARRaycastManager))]
+[RequireComponent(typeof(ARPlaneManager))]
 
-
-public class ARManager: MonoBehaviour
+public class ARManager : MonoBehaviour
 {
     public GameObject currentObject;
 
@@ -20,23 +20,39 @@ public class ARManager: MonoBehaviour
         hammer,
     }
 
+    private enum arState
+    {
+        placeItem,
+        interact,
+        move,
+    }
+
+
+    private arState currentState = arState.placeItem;
     public toolList currentTool = toolList.glove;
     private GameObject spawmedObject;
     private ARRaycastManager m_arCastManager;
     private Vector2 touchPosition;
     private ARSessionOrigin arOrigin;
-    private bool isLocked = false;
-  ///  public GameObject particles;
-  //  public GameObject popUP;
+    private ARPlaneManager planeManager;
+
+    private GameObject selectedObject = null;
+
+
     public List<string> clueList;
     private List<string> foundClues = new List<string>();
 
     static List<ARRaycastHit> hits = new List<ARRaycastHit>();
-
     private bool isUVLightOn = false;
     public GameObject UVLight;
-    private GameObject lightTarget;
-    public GameObject lightDistanceUI;
+
+    public GameObject placeUI;
+    public GameObject interactUI;
+    public GameObject moveUI;
+
+    public Shader outlineShader;
+    public Shader standardShader;
+
 
     private void Start()
     {
@@ -47,7 +63,8 @@ public class ARManager: MonoBehaviour
     private void Awake()
     {
         m_arCastManager = GetComponent<ARRaycastManager>();
-        //particles.SetActive(false);
+        planeManager = GetComponent<ARPlaneManager>();
+
     }
 
     bool TryGetTouchPosition(out Vector2 touchPosition)
@@ -88,32 +105,31 @@ public class ARManager: MonoBehaviour
         currentTool = toolList.hammer;
     }
 
-
-
-    public void Lock()
+    private void changeState(arState newState)
     {
-        if (isLocked)
-        {
-            isLocked = false;
-        }
-        else
-        {
-            isLocked = true;
-        }
-
 
     }
+
 
     public void SetObject(GameObject p_object)
     {
         Destroy(spawmedObject);
 
         currentObject = p_object;
-        MoveObject();
+        // MoveObject();
     }
 
 
-    private void MoveObject()
+    private void SetAllPlanesActive(bool value){
+
+        foreach (var plane in planeManager.trackables)
+        {
+            plane.gameObject.SetActive(value);
+        }
+
+        }
+
+    private void PlaceObject()
     {
 
         if (m_arCastManager.Raycast(new Vector2(Screen.width / 2, Screen.height / 2), hits, TrackableType.PlaneWithinPolygon))
@@ -123,7 +139,8 @@ public class ARManager: MonoBehaviour
             if (spawmedObject == null)
             {
                 spawmedObject = Instantiate(currentObject.gameObject, hitPos.position, hitPos.rotation);
-
+                // spawmedObject.GetComponent<MeshRenderer>().material.color = new Color(1.0f,1.0f,1.0f,0.5f);
+            
             }
             else
             {
@@ -143,30 +160,151 @@ public class ARManager: MonoBehaviour
 
     }
 
-
-    void CheckObjectCollisions()
+    public void placeObject()
     {
-        TryGetTouchPosition(out touchPosition);
-        Ray ray = Camera.main.ScreenPointToRay(touchPosition);
-        RaycastHit hitObject;
-        if (Physics.Raycast(ray, out hitObject, 25.0f))
-        {
-            if (hitObject.transform.tag == "Selectable")
-            {
-                hitObject.transform.GetComponent<ClueBehaviour>().onCollision(currentTool);
+        //spawmedObject.GetComponent<MeshRenderer>().material.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
 
-                //particles.SetActive(true);
-                //particles.transform.position = hitObject.point;
+        if (spawmedObject != null)
+        {
+            currentState = arState.interact;
+            planeManager.enabled = !planeManager.enabled;
+            SetAllPlanesActive(false);
+            placeUI.SetActive(false);
+            interactUI.SetActive(true);
+        }
+        
+
+    }
+ 
+    private void CheckObjectSelection()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            touchPosition = touch.position;
+            bool isOverUI = IsPointerOverUIObject(touchPosition);
+
+
+
+            if (!isOverUI)
+            {
+                if (touch.phase == TouchPhase.Began)
+                {
+                    Ray ray = Camera.main.ScreenPointToRay(touch.position);
+                    RaycastHit hitObject;
+                    if (Physics.Raycast(ray, out hitObject))
+                    {
+                        if (selectedObject != null)
+                        {
+
+                        }
+                        selectedObject = hitObject.transform.gameObject;
+                        // selectedObject.GetComponent<Renderer>().material.shader = outlineShader;
+                        for (int i = 0; i < spawmedObject.transform.childCount; i++)
+                        {
+                            
+                                if (selectedObject.transform.GetChild(i).tag != "Selectable")
+                            {
+                                selectedObject.transform.GetChild(i).GetComponent<Renderer>().material.shader = outlineShader;
+                            }
+                           
+                        }
+
+                        //Renderer[] children;
+                        //children = GetComponentsInChildren<Renderer>();
+                        //foreach (Renderer rend in children)
+                        //{
+                        //    var mats = new Material[rend.materials.Length];
+                        //    for (var j = 0; j < rend.materials.Length; j++)
+                        //    {
+                        //        mats[j].shader = outlineShader;
+                        //    }
+                        //    rend.materials = mats;
+                        //}
+
+                        currentState = arState.move;
+                        interactUI.SetActive(false);
+                        moveUI.SetActive(true);
+                        isUVLightOn = false;
+                        UVLight.SetActive(false);
+
+                    }
+
+                }
+
             }
         }
-        else
-        {
-           // particles.SetActive(false);
-        }
-
 
 
     }
+
+  
+
+
+
+        private void MoveObject()
+    {
+        if (Input.touchCount == 1)
+        {
+            Touch touch = Input.GetTouch(0);
+
+            var currentTouch = touch.position;
+
+            bool isOverUI = IsPointerOverUIObject(currentTouch);
+
+
+
+            if (!isOverUI)
+            {
+                if (m_arCastManager.Raycast(currentTouch, hits, TrackableType.PlaneWithinPolygon))
+                {
+                    var hitPos = hits[0].pose;
+
+
+                    selectedObject.transform.position = hitPos.position;
+
+
+                }
+            }
+        }
+        if (Input.touchCount == 2)
+        {
+            // Store both touches.
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            // Find the position in the previous frame of each touch.
+            Vector2 touchZeroPrevPos = touchZero.position - touchZero.deltaPosition;
+            Vector2 touchOnePrevPos = touchOne.position - touchOne.deltaPosition;
+
+            // Find the magnitude of the vector (the distance) between the touches in each frame.
+            float prevTouchDeltaMag = (touchZeroPrevPos - touchOnePrevPos).magnitude;
+            float touchDeltaMag = (touchZero.position - touchOne.position).magnitude;
+
+            // Find the difference in the distances between each frame.
+            float deltaMagnitudeDiff = prevTouchDeltaMag - touchDeltaMag;
+            float pinchAmount = deltaMagnitudeDiff * 0.002f * Time.deltaTime;
+            selectedObject.transform.localScale -= new Vector3(pinchAmount, pinchAmount, pinchAmount);
+        }
+    }
+
+    private bool IsPointerOverUIObject(Vector2 pos)
+    {
+        if (EventSystem.current.IsPointerOverGameObject())
+        {
+            return false;
+        }
+        PointerEventData evenPosition = new PointerEventData(EventSystem.current);
+        evenPosition.position = new Vector2(pos.x, pos.y);
+
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(evenPosition, results);
+
+        return results.Count > 0;
+
+    }
+
 
     public void ActivatePopUp(bool isFound)
     {
@@ -231,8 +369,21 @@ public class ARManager: MonoBehaviour
         }
 
     }
-
-
+    //locks and removes the selected object
+    public void Lock()
+    {
+        moveUI.SetActive(false);
+        interactUI.SetActive(true);
+        currentState = arState.interact;
+        for (int i = 0; i < spawmedObject.transform.childCount; i++)
+        {
+            if (selectedObject.transform.GetChild(i).tag != "Selectable")
+            {
+                selectedObject.transform.GetChild(i).GetComponent<Renderer>().material.shader = standardShader;
+            }
+        }
+        selectedObject = null;
+    }
 
     // Update is called once per frame
     void Update()
@@ -240,49 +391,17 @@ public class ARManager: MonoBehaviour
         if (!currentObject)
             return;
 
-        if (!isLocked)
+        if (currentState == arState.placeItem)
+        {
+            PlaceObject();
+        }
+        else if (currentState == arState.interact)
+        {
+            CheckObjectSelection();
+        }
+        else if (currentState == arState.move)
         {
             MoveObject();
-        }
-        else
-        {
-
-
-
-            if (lightTarget)
-            {
-                lightTarget.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, 1 - (Vector3.Distance(lightTarget.transform.position, this.transform.position) - 0.5f));
-                lightDistanceUI.GetComponent<UnityEngine.UI.Text>().text = Vector3.Distance(lightTarget.transform.position, this.transform.position).ToString();
-            }
-            if (isUVLightOn)
-            {
-                RaycastHit hitObject;
-                Ray ray = Camera.main.ScreenPointToRay(new Vector3(Camera.main.pixelWidth / 2f, Camera.main.pixelHeight / 2f, 0f));
-
-                if (Physics.Raycast(ray, out hitObject, 25.0f))
-                {
-
-                    if (hitObject.transform.tag == "Selectable")
-                    {
-
-                        if (lightTarget != hitObject.transform.gameObject)
-                        {
-                            // lightTarget.transform.GetComponent<SpriteRenderer>().enabled = false;
-                            lightTarget = hitObject.transform.gameObject;
-
-                        }
-
-                        lightTarget.GetComponent<SpriteRenderer>().enabled = true;
-                        lightTarget.GetComponent<SpriteRenderer>().color = new Color(1, 1, 1, (Vector3.Distance(lightTarget.transform.position, this.transform.position) - 0.5f));
-                        lightDistanceUI.GetComponent<UnityEngine.UI.Text>().text = Vector3.Distance(lightTarget.transform.position, this.transform.position).ToString();
-                    }
-                }
-
-
-            }
-            CheckObjectCollisions();
-
-
         }
 
     }
