701311f2d9b9cbfe46fac6f6860ed3614bf21597
Alessandro Iop <AleIop@users.noreply.github.com>
2020-12-10 13:15:35 +0100

    Content list for each page
    Corrected responsiveness of main title + copied strucutre to all pages + listed each page's content

--- js/index.js
+++ js/index.js
@@ -5,18 +5,6 @@ $(document).ready(function(){
     //Sidenav init
     $('.sidenav').sidenav();
 
-    //Tabs init
-    $('.tabs').tabs();
-
-    //Modal init
-    $('.modal').modal();
-
-    //Feature discovery init: menu button
-    var elemsTap = document.querySelector('#tap-menu');
-    var instancesTap = M.TapTarget.init(elemsTap, {});
-    instancesTap.open();
-    setTimeout(() => instancesTap.close(), 3000);
-
 	//Check to see if the window is top if not then display button
     $(window).scroll(function(){
         if ($(this).scrollTop() > 150) {
