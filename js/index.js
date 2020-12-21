4c2717fc00ec6ac93b52f7ba849f8f25ad326303
Alessandro Iop <AleIop@users.noreply.github.com>
2020-12-21 11:06:42 +0100

    Popup modal for warning

--- js/index.js
+++ js/index.js
@@ -8,11 +8,11 @@ $(document).ready(function(){
     //Images init
     $('.materialboxed').materialbox();
 
-    //Pushpin init
-    $('.target').pushpin();
-
     //Carousel init
     $('.carousel').carousel();
+
+    //Modal init
+    $('.modal').modal();
 
 	//Check to see if the window is top if not then display button
     $(window).scroll(function(){
@@ -27,15 +27,5 @@ $(document).ready(function(){
     $('.scrollToTop').click(function(){
         $('html, body').animate({scrollTop : 0},800);
         return false;
-    });
-
-    //Scroll to replace navbar
-    $('.pushpin-demo-nav').each(function() {
-        var $this = $(this);
-        var $target = $('#' + $(this).attr('data-target'));
-        $this.pushpin({
-            top: $target.offset().top,
-            bottom: $target.offset().top + $target.outerHeight() - $this.height()
-        });
     });
 });
